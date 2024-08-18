2f249f499e53a93815cc306989ae33b34255217c
Ulan Bekboev <ubekboouulu@gmail.com>
2024-08-18 20:53:20 +0600

    Изменяет путь к скрипту

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const autoprefixer = require('autoprefixer');
 const mediaquery = require('postcss-combine-media-query');
 const cssnano = require('cssnano');
 const htmlMinify = require('html-minifier');
+const concatjs = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 function html() {
   const options = {
@@ -53,6 +55,17 @@ function images() {
     .pipe(browserSync.reload({ stream: true }));
 }
 
+function scripts() {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(plumber()) 
+    .pipe(concatjs('bundle.js')) 
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+
 function clean() {
   return del('dist');
 }
@@ -60,6 +73,7 @@ function clean() {
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/**/*.css'], css);
+  gulp.watch(['src/**/*.js'], scripts);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
@@ -71,13 +85,14 @@ function serve() {
   });
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, scripts));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.clean = clean;
 exports.images = images;
 exports.css = css;
 exports.html = html;
+exports.scripts = scripts;
 
 exports.build = build;
 exports.watchapp = watchapp;
